f34895fe1b4e4800ea32c630c029e4da55334fe1
febinakamalbscphysics-stack <febinakamalbscphysics@gmail.com>
2025-09-24 15:33:41 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,18 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function insert(value) {
+    document.getElementById("result").value += value;
+}
+
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+function deleteLast() {
+    var p = document.getElementById("result").value;
+    document.getElementById("result").value = p.slice(0, -1);
+}
